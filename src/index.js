2bb2b12a07543a0f818137d69733c1abee077d97
zhouhong07 <zhouhong07@58.com>
2020-03-12 16:04:32 +0800

    添加样式到index

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@
  * @Author: zhouhong07
  * @Date: 2020-03-04 15:50:25
  * @LastEditors: zhouhong07
- * @LastEditTime: 2020-03-11 15:15:06
+ * @LastEditTime: 2020-03-12 16:03:25
  */
 import React from 'react';
 import ReactDom from 'react-dom';
 import App from './App.js';
 import './demo.ts' ;
-
+import './index.scss';
 ReactDom.render(<App />, document.getElementById('root'));
